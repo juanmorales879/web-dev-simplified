de78ebd53f536bd3b92540a93f09819b5a5604ec
juanmorales879 <juanmorales.marcano@gmail.com>
2025-10-30 21:17:09 -0700

    midi piano

---
+++ Midi piano/script.js
@@ -0,0 +1,24 @@
+const audioContext = new AudioContext();
+
+const piano = document.querySelector(".piano")
+
+const NOTE_DETAILS = [
+  { note: "C", key: "Z", frequency: 261.626, active:false},
+  { note: "Db", key: "S", frequency: 277.183, active:false },
+  { note: "D", key: "X", frequency: 293.665, active:false },
+  { note: "Eb", key: "D", frequency: 311.127, active:false },
+  { note: "E", key: "C", frequency: 329.628, active:false },
+  { note: "F", key: "V", frequency: 349.228, active:false},
+  { note: "Gb", key: "G", frequency: 369.994, active:false },
+  { note: "G", key: "B", frequency: 391.995, active:false },
+  { note: "Ab", key: "H", frequency: 415.305, active:false },
+  { note: "A", key: "N", frequency: 440, active:false },
+  { note: "Bb", key: "J", frequency: 466.164, active:false },
+  { note: "B", key: "M", frequency: 493.883, active:false }
+]
+
+document.addEventListener( "keydown", e => 
+{
+    console.log(e)
+}
+)
