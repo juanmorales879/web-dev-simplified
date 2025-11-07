3b916bd01bf9de0a7e5605acf6e34b7a83be1bfe
juanmorales879 <juanmorales.marcano@gmail.com>
2025-11-06 22:04:33 -0400

    form validation, final commit

--- form validation/script.js
+++ form validation/script.js
@@ -6,11 +6,12 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const passwordConfirmation = document.querySelector("#password-confirmation");
 const terms = document.querySelector("#terms");
-let errorMessages = [];
 const requiredUsernameLength = 6;
 const requiredPasswordLength = 10;
 
 form.addEventListener("submit", (e) => {
+  const errorMessages = [];
+
   clearErrors();
 
   if (username.value.length < requiredUsernameLength) {
@@ -31,21 +32,18 @@ form.addEventListener("submit", (e) => {
 
   if (errorMessages.length > 0) {
     e.preventDefault();
-    showErrors();
+    showErrors(errorMessages);
   }
 });
 
-// TODO: Define this function
 function clearErrors() {
-  // Loop through all the children of the error-list element and remove them
-  // IMPORTANT: This cannot be done with a forEach loop or a normal for loop since as you remove children it will modify the list you are looping over which will not work
-  // I recommend using a while loop to accomplish this task
-  // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
-  // Also, make sure you remove the show class to the errors container
+  while (errorList.firstChild) {
+    errorList.removeChild(errorList.firstChild);
+  }
+  errorBox.classList.remove("show");
 }
 
-// TODO: Define this function
-function showErrors() {
+function showErrors(errorMessages) {
   errorMessages.forEach((error) => {
     const newLi = document.createElement("li");
     newLi.textContent = error;
