fcde12cc94d336895c873d2ee5953fc69bd50efd
juanmorales879 <juanmorales.marcano@gmail.com>
2025-11-12 21:52:46 -0400

    new items

--- Advanced to do/script.js
+++ Advanced to do/script.js
@@ -1,1 +1,17 @@
-console.log("working ");
+const template = document.querySelector("#list-item-template");
+const form = document.querySelector("#new-todo-form");
+const input = document.querySelector("#todo-input");
+//const list = document.querySelector(".list-item");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = input.value.trim();
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("[data-list-item-text]").textContent = inputValue;
+  list.append(clone);
+
+  clone.addEventListener("click", (e) => {
+    console.log(e.target);
+    console.log("inside clone");
+  });
+});
