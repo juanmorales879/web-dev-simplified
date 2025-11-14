67fab792f1289315e1479a95b88c09db50392436
juanmorales879 <juanmorales.marcano@gmail.com>
2025-11-13 23:05:40 -0400

    all done

--- Advanced to do/script.js
+++ Advanced to do/script.js
@@ -12,15 +12,10 @@ saveTasks();
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const inputValue = input.value.trim();
-  tasks.push(inputValue);
-  addLi(inputValue);
+  const newTask = { id: Math.random(), task: inputValue };
+  tasks.push(newTask);
+  addLi(newTask);
   saveTasks();
-  list.addEventListener("click", (e) => {
-    if (e.target.matches("[data-button-delete]")) {
-      const elDelete = e.target.closest(".list-item");
-      elDelete.remove();
-    }
-  });
 });
 
 function saveTasks() {
@@ -29,6 +24,21 @@ function saveTasks() {
 
 function addLi(input) {
   const clone = template.content.cloneNode(true);
-  clone.querySelector("[data-list-item-text]").textContent = input;
+  const li = clone.querySelector("[data-list-item-text]");
+  li.textContent = input.task;
+  li.dataset.id = input.id;
   list.append(clone);
 }
+
+list.addEventListener("click", (e) => {
+  if (e.target.matches("[data-button-delete]")) {
+    const elDelete = e.target.closest(".list-item");
+    const span = elDelete.querySelector("[data-list-item-text]");
+    const id = Number(span.getAttribute("data-id"));
+    console.log(id);
+    elDelete.remove();
+    const deleteObject = tasks.filter((number) => number.id !== id);
+    console.log(deleteObject);
+    localStorage.setItem("item", JSON.stringify(deleteObject));
+  }
+});
