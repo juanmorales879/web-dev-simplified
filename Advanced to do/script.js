ee701bc25d8b16613eaf8d1b7c0fdd1b3a094517
juanmorales879 <juanmorales.marcano@gmail.com>
2025-11-13 21:39:15 -0400

    adding localstorage

--- Advanced to do/script.js
+++ Advanced to do/script.js
@@ -2,14 +2,15 @@ const template = document.querySelector("#list-item-template");
 const form = document.querySelector("#new-todo-form");
 const input = document.querySelector("#todo-input");
 const list = document.querySelector("#list");
+let tasks = localStorage.getItem("item")
+  ? JSON.parse(localStorage.getItem("item"))
+  : [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const inputValue = input.value.trim();
-  const clone = template.content.cloneNode(true);
-  clone.querySelector("[data-list-item-text]").textContent = inputValue;
-  list.append(clone);
-
+  addLi(inputValue);
+  console.log(tasks);
   list.addEventListener("click", (e) => {
     if (e.target.matches("[data-button-delete]")) {
       const elDelete = e.target.closest(".list-item");
@@ -17,3 +18,14 @@ form.addEventListener("submit", (e) => {
     }
   });
 });
+
+function save() {
+  tasks.foreach(addLi);
+}
+
+function addLi(input) {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("[data-list-item-text]").textContent = input;
+  list.append(clone);
+  localStorage.setItem("item", JSON.stringify(tasks));
+}
