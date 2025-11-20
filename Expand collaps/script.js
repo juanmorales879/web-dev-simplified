4f1811ed84cd41385f98cb0aa57f0c79bc3b72cf
juanmorales879 <juanmorales.marcano@gmail.com>
2025-11-19 21:39:07 -0400

    all done

--- Expand collaps/script.js
+++ Expand collaps/script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const cardBody = document.querySelectorAll(".card-body");
+const buttons = document.querySelectorAll(".expand-button");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    cardSelected = e.target.closest(".card").querySelector(".card-body");
+    cardSelected.classList.toggle("show");
+    const buttonState = button.innerText;
+
+    if (buttonState === "Collapse") {
+      button.innerText = "Expand";
+    } else {
+      button.innerText = "Collapse";
+    }
+  });
+});
